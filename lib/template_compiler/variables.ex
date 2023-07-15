66215ac7d78fd43aa297ff4efe57336acf92c834
Thomas Timmer <thomastimmer11@hotmail.com>
2023-07-15 20:40:59 +0200

    feat: add data loader

--- lib/template_compiler/variables.ex
+++ lib/template_compiler/variables.ex
@@ -3,17 +3,20 @@ defmodule TemplateCompiler.Variables do
   Variables that the generated Rust project expects
   """
   defstruct app: "my_templater_1234",
-            template_path: "hello.html",
+            template_path: "data_table.html.jinja",
             message: "BIEM!",
             struct: "MyTemplate",
-            struct_items: %{name: "String", age: "usize"}
+            struct_items: %{name: "String", sort: "String"},
+            data_url: {:get, "https://raw.githubusercontent.com/thomas9911/csv-generator/main/data/1000.json"}
 
+  @type request_method :: :get | :post
   @type t :: %__MODULE__{
           app: binary,
           message: binary,
           struct: binary,
           struct_items: map,
-          template_path: binary
+          template_path: binary,
+          data_url: {request_method, binary}
         }
 
   @spec default() :: t()
